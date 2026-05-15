4ce5af87836ef6e5556437598d680ba2a291ffe7
kurio2510 <megannivip200345@gmail.com>
2026-05-16 00:04:19 +0700

    Updated
    Added functionality to check divisibility by 3 and 5.

--- btvn/(3).py
+++ btvn/(3).py
@@ -1,5 +1,7 @@
-number = float(input("Nhập một số thập phân: "))
-
-integer_number = int(number)
-
-print("Số nguyên sau khi chuyển đổi là:", integer_number)
+n = int(input("Nhap mot so nguyen: "))
+if n % 3 == 0 and n % 5 == 0:
+    print("So vua nhap chia het cho ca 3 va 5")
+elif n % 3 == 0:
+    print("So vua nhap chia het cho 3")
+elif n % 5 == 0:
+    print("So vua nhap chia het cho 5")
