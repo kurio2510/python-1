4ecf397b153881b60e41237abd59d70a8e73bea8
kurio2510 <megannivip200345@gmail.com>
2026-05-16 00:04:55 +0700

    Updated
    Add functionality to check if a number is a palindrome.

--- btvn/(4).py
+++ btvn/(4).py
@@ -1,5 +1,10 @@
-number_str = input("Nhập một chuỗi số thập phân: ")
-
-number_float = float(number_str)
-
-print("Số thực sau khi chuyển đổi là:", number_float)
+n = int(input("Nhap mot so nguyen duong: "))
+temp = n
+so_dao_nguoc = 0
+while temp > 0:
+    so_dao_nguoc = so_dao_nguoc * 10 + temp % 10
+    temp //= 10
+if n == so_dao_nguoc:
+    print("So vua nhap la so doi xung")
+else:
+    print("So vua nhap khong phai la so doi xung")
