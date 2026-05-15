bf92ca88fbad585e6a9520b1e192b7fec53429da
kurio2510 <megannivip200345@gmail.com>
2026-05-16 00:03:11 +0700

    Updated

--- btvn/(1).py
+++ btvn/(1).py
@@ -1,3 +1,7 @@
-name = input("Nhập tên của bạn: ")
-
-print("Xin chào", name)
+n = int(input("Nhap mot so nguyen: "))
+if n < 0:
+    print("So vua nhap la so am")
+elif n > 0:
+    print("So vua nhap la so duong")
+else:
+    print("So vua nhap la so 0")
