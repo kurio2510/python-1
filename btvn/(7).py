648f06da0b1822ce54c6bfeec94cf114b3dd41bd
kurio2510 <megannivip200345@gmail.com>
2026-05-16 00:06:14 +0700

    Updated

---
+++ btvn/(7).py
@@ -0,0 +1,7 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+if a + b > c and a + c > b and b + c > a:
+    print("La 3 canh tam giac")
+else:
+    print("Khong phai 3 canh tam giac")
